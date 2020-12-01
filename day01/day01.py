3d3e026f1d48dc650e4149f330ec1fad60fec262
europa9 <siditlef@gmail.com>
2020-12-01 17:22:03 +0100

    Rot24

--- day01/day01.py
+++ day01/day01.py
@@ -1,3 +1,26 @@
 
+# This class should do ROT24
+
 class Day01:
     print('Day 01')
+
+    access_code = 'AaBbRUV{JgkJqPåGtFgvLwnKilgp}'
+    alphabeth_lowercase = 'abcdefghijklmnopqrstuvwxyzæøå'
+    alphabeth_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ'
+
+    # Iterate over index
+    #for element in range(0, len(access_code)):
+     #   letter = access_code[element]
+
+    for access_code_character in access_code:
+
+
+        # Lowercase find character number, a=1, b=2, c=3 etc
+        x = 0;
+        for find_char in alphabeth_lowercase:
+            x = x+1
+            if(find_char == access_code_character):
+                print("Char " + str(access_code_character) + " is number " + str(x) + " in alpabeth")
+
+                switch_with_number = x+24
+
--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 # Date: 24.11.2020
 # Version: 1.0
 
-import day01
+from day01.day01 import Day01
+
 
 class Main:
-    print('NPST 2020')
+    print('~~ NPST 2020 ~~')
 
-
-
+    # Comment away whatever you dont want
+    day01 = Day01()
