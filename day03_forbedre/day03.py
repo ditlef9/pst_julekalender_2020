cf284236bf3ac6ddf03cec9384df3c65f4579e01
Sindre Andre <sindred@outlook.com>
2020-12-03 14:34:38 +0100

    Day03

---
+++ day03_forbedre/day03.py
@@ -0,0 +1,10 @@
+# Filename: day03_forbedre.py
+# Author: S Ditlefsen
+# License: http://opensource.org/licenses/gpl-license.php GNU Public License
+
+# Assignment:
+#   Din kollega Tastefinger har identifisert noe 🧁 med fila cupcake.png fra beslaget du arbeidet med i går. Det er SANNSYNLIG at det kan være informasjon i bildet som ikke er synlig med det blotte øye. Gleder meg til å høre hva du kommer frem til!
+#
+# Solution:
+#   Upload cupcake.png to DASS by going to "Start -> Forbedre". The solution appears.
+#
