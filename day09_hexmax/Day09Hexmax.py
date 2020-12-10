0ff8a70ec8743f58569351d064a01c26e0d7e51c
europa9 <siditlef@gmail.com>
2020-12-10 17:15:16 +0100

    Day09

--- day09_hexmax/Day09Hexmax.py
+++ day09_hexmax/Day09Hexmax.py
@@ -1,4 +1,4 @@
-# Filename: Day09Hexmax.py
+# Filename: Day09Hexmax_old.py
 # Author: S Ditlefsen
 # License: http://opensource.org/licenses/gpl-license.php GNU Public License
 
@@ -13,6 +13,7 @@
 #     🎅🤶❄⛄🎄🎁🕯🌟✨🔥🥣🎶🎆👼🦌🛷
 #
 #     🤶🛷✨🎶🎅✨🎅🎅🛷🤶🎄🔥🎆🦌🎁🛷🎅❄🛷🛷🎅🎶🎅✨🎅🦌🥣🔥🛷🦌⛄🎅🌟🛷🛷🔥🎄🦌🎅✨🦌🦌🕯🎶🎅🤶🦌❄🎁🕯🎅✨🎶👼🌟🎆🕯🌟❄👼🎅🎅🤶❄🎄👼🎆🔥🎁🛷🤶👼🎅🎅🎅🎅🎅🎅
+import binascii
 from collections import Counter
 
 
@@ -21,89 +22,56 @@ class Day09Hexmax:
     print('~~~~~~~~~~~~~~~~~~~~~~~~ Day 09 ~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
-    def printOverview():
+    def breakTheCode():
 
         # Code to break
         secret_code = "🤶🛷✨🎶🎅✨🎅🎅🛷🤶🎄🔥🎆🦌🎁🛷🎅❄🛷🛷🎅🎶🎅✨🎅🦌🥣🔥🛷🦌⛄🎅🌟🛷🛷🔥🎄🦌🎅✨🦌🦌🕯🎶🎅🤶🦌❄🎁🕯🎅✨🎶👼🌟🎆🕯🌟❄👼🎅🎅🤶❄🎄👼🎆🔥🎁🛷🤶👼🎅🎅🎅🎅🎅🎅"
 
         # Input
         alphabet_emoji = "🎅🤶❄⛄🎄🎁🕯🌟✨🔥🥣🎶🎆👼🦌🛷"
-        alphabet_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        alphabet_hex = "0123456789ABCDEF"
 
 
         # Connect emoji to alphabet
         print("\n(1) Emoji to alphabet")
         print("Length: " + str(len(alphabet_emoji)))
-        print("Emoji\tUnicode\tOccurrence\tLetter")
+        print("Emoji\tNumber (Hex)")
         counter = Counter(alphabet_emoji)
         replacement_dictionary = dict()
         x = 0
         for emoji in alphabet_emoji:
-            unicode = f'U+{ord(emoji):X}'
-            occurrence = counter[emoji]
-            letter = alphabet_uppercase[x]
-            print(emoji + "\t\t" + unicode + "\t\t" + str(occurrence) + "\t\t" + letter)
+            hex = alphabet_hex[x]
+            print(emoji + "\t\t" + "\t\t" + str(hex))
 
             # Add to dictionary
-            replacement_dictionary[emoji] = str(letter);
+            replacement_dictionary[emoji] = str(hex);
 
             x = x+1
 
 
-        # Break the code
-        print("\n(2) Break the code")
+        # Replace secret_code emoji to hex from alphabet
+        print("\n(2) Replace secret code emoji to hex from alphabet")
         print("Length: " + str(len(secret_code)))
-        print("Emoji\tUnicode\tOccurrence\tReplaced")
+        print("Emoji\t\tReplaced with\t\tOccurrence")
         counter = Counter(secret_code)
         x = 0
         solution = ''
         for emoji in secret_code:
-            unicode = f'U+{ord(emoji):X}'
             occurrence = counter[emoji]
             replaced_character = replacement_dictionary[emoji]
 
-            print(emoji + "\t\t" + unicode + "\t\t" + str(occurrence) + "\t\t" + replaced_character)
-
-            x=x+1
-
-    def solveCode():
-        # Replacement alpabet for 🎅🤶❄⛄🎄🎁🕯🌟✨🔥🥣🎶🎆👼🦌🛷
-        replacement_dictionary = dict()
-        replacement_dictionary['🎅'] = 'A' #1
-        replacement_dictionary['🤶'] = 'B' #2
-        replacement_dictionary['❄'] = 'C' #3
-        replacement_dictionary['⛄'] = '}' #4 ok
-        replacement_dictionary['🎄'] = 'E' #5
-        replacement_dictionary['🎁'] = 'F' #6
-        replacement_dictionary['🕯'] = 'G' #7
-        replacement_dictionary['🌟'] = 'H' #8
-        replacement_dictionary['✨'] = 'I' #9
-        replacement_dictionary['🔥'] = 'J' #10
-        replacement_dictionary['🥣'] = '{' #11 ok
-        replacement_dictionary['🎶'] = 'L' #12
-        replacement_dictionary['🎆'] = 'M' #13
-        replacement_dictionary['👼'] = 'N' #14
-        replacement_dictionary['🦌'] = 'O' #15
-        replacement_dictionary['🛷'] = 'P' #16
-
-
-
-
-        # Break the code
-        secret_code = "🤶🛷✨🎶🎅✨🎅🎅🛷🤶🎄🔥🎆🦌🎁🛷🎅❄🛷🛷🎅🎶🎅✨🎅🦌🥣🔥🛷🦌⛄🎅🌟🛷🛷🔥🎄🦌🎅✨🦌🦌🕯🎶🎅🤶🦌❄🎁🕯🎅✨🎶👼🌟🎆🕯🌟❄👼🎅🎅🤶❄🎄👼🎆🔥🎁🛷🤶👼🎅🎅🎅🎅🎅🎅"
-        print("\nBreak with known alphabet")
-        counter = Counter(secret_code)
-        x = 0
-        solution = ''
-        for emoji in secret_code:
-            unicode = f'U+{ord(emoji):X}'
-            occurrence = counter[emoji]
-            replaced_character = replacement_dictionary[emoji]
-
-            print(replaced_character, end='')
-
+            print(emoji + "\t\t" + replaced_character + "\t\t" + str(occurrence))
+            solution = solution + replaced_character;
             x=x+1
 
 
-    printOverview()
-    solveCode()
+        # Solution to zip
+        print("\n\n(3) Hex solution code is:")
+        print(solution)
+        print("\nCopy the code and open HxD program. Create new file. Paste the code into HxD.")
+        print("Save the file as solution.gz. Open solution.gz in WinRar. You now got the solution.")
+        print("Google 1F8B08.")
+
+
+
+    breakTheCode()
